3baac8cc22e99a9d019cca0f67db45ec79abb42f
Colegiales32 <103970514+Colegiales32@users.noreply.github.com>
2024-05-28 21:09:47 -0300

    agregado de scrollIntoView a experiencias

--- src/pages/Experiencias.jsx
+++ src/pages/Experiencias.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Nav from '../components/Nav';
 import { useTranslation } from 'react-i18next';
 import Footer from '../components/Footer';
@@ -9,9 +9,13 @@ import Wpp from '../components/Wpp';
 const Experiencias = () => {
   const [expandidoId, setExpandidoId] = useState();
   const [t, i18next] = useTranslation("global")
+  const tituloRef = useRef(null);
   
   const handleExpandido = (id) => {
     setExpandidoId(id);
+    if (tituloRef.current) {
+      tituloRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
   };
   const handleCierre = (e) => {
     e.stopPropagation();
@@ -25,7 +29,7 @@ const Experiencias = () => {
             <div className='bg-fondo bg-no-repeat bg-cover min-h-screen'>
                 <Nav/>
                 
-                <h1 className='text-center font-stencil text-4xl my-6 font-bold '>{t("experiencias.titulo")}</h1>
+                <h1 ref={tituloRef} className='text-center font-stencil text-4xl my-6 font-bold '>{t("experiencias.titulo")}</h1>
                 
                 <div className='h-screen'>
                       <div className='relative flex flex-col lg:items-start pb-10 lg:flex-row w-4/5 gap-3 lg:gap-5 justify-between h-svh lg:h-4/5 mx-auto'>
